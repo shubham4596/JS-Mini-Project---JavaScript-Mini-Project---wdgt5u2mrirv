77fbf74c47ee6fe43d375af3cf9f95c906d4acf4
shubham4596 <104636433+shubham4596@users.noreply.github.com>
2022-07-10 13:47:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
-
+function validate(){
+    var username= document.getElementById("username").value;
+    var password= document.getElementById("password").value;
+    if(username=="shubham" && password=="123456"){
+        alert("you have successfull login");
+        return false
+    }
+    else{
+        alert("please enter valid credentials");
+    }
+}
